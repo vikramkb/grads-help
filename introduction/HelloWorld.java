7f9173455bd9f62fc4f325eeaf252fd677ec2109
vikramkb <vikram.kuruguntla@gmail.com>
2022-11-01 20:56:23 +0530

    Delete introduction directory

--- introduction/HelloWorld.java
+++
@@ -1,7 +0,0 @@
-package introduction;
-
-public class HelloWorld {
-    public static void main(String[] args) throws InterruptedException {
-        System.out.println("Welcome to Hello World");
-    }
-}
